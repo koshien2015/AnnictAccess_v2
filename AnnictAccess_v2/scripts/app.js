6506e5ac3faa232b7000a8d6a20f553c755bed37
ckoshien <ckoshien@gmail.com>
2017-09-05 20:07:27 +0900

    修正。グラフ実装中

--- AnnictAccess_v2/scripts/app.js
+++ AnnictAccess_v2/scripts/app.js
@@ -1,6 +1,6 @@
 //var app = angular.module('myApp', []);
 'use strict';
-angular.module('myApp', ['ui.router','angular-loading-bar', 'ngAnimate'])
+angular.module('myApp', ['ui.router','angular-loading-bar', 'ngAnimate','highcharts-ng'])
 .config(["$locationProvider", function ($locationProvider) {
 }])
 // Routes
@@ -28,7 +28,34 @@ angular.module('myApp', ['ui.router','angular-loading-bar', 'ngAnimate'])
 .controller('authCtrl', function($scope, $stateParams,$http,$rootScope,$location) {
 	var host = $location.host();
 	var port = $location.port();
-
+	$scope.chartConfig = {
+		chart: {
+			type: 'column'
+		},
+		title:{
+			text:'アニメ未消化状況'
+		},
+		xAxis: [{
+            categories: []
+        }],
+        yAxis: {
+			title: {
+				text: null
+			},
+				labels: {
+					style: {
+						color: '#000000'
+					}
+				},
+				floor: 0,
+				allowDecimals:true,
+				startOnTick: false
+		},
+		series: [{
+			name: '未消化数',
+			data: []
+		}]
+	}
 	$http({
 		method: 'GET',
 		headers: {
@@ -36,8 +63,25 @@ angular.module('myApp', ['ui.router','angular-loading-bar', 'ngAnimate'])
 		},
 	    url: 'https://api.annict.com/v1/me/programs?sort_started_at=asc&per_page=40&filter_unwatched=true&accessToken='+$stateParams.code
 	})
-
+	.success(function(data, status, headers, config,chart) {
+		$scope.items = data;
+		for(var i=0;i<$scope.items.programs.length;i++){
+			var dt = new Date($scope.items.programs[i].started_at);
+			var now = new Date();
+			$scope.items.programs[i]['dayDiff']=Math.floor((now-dt)/(1000 * 60 * 60 * 24));
+			//chart.series[0].addPoint($scope.items.programs[i].started_at,true,shift);
+			$scope.chartConfig.xAxis[0].categories.push($scope.items.programs[i].started_at);
+		}
+	})
 })
 .controller('indexCtrl',['$scope',function($scope) {
 
 }])
+
+function dateFormatString(date,fmt){
+	var format = {
+			'YYYY': function() { return padding(date.getFullYear(), 4, pad); },
+	        'MM': function() { return padding(date.getMonth()+1, 2, pad); },
+	        'DD': function() { return padding(date.getDate(), 2, pad); },
+	    };
+}
